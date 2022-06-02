28be7e1671d88f5ce2299c5cb5d99e2808f8f787
erwanor <erwan.ounn.84@gmail.com>
2022-06-02 17:55:47 +0200

    test(commit): check that trapdoor is additively homomorphic

--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,7 @@ pub struct Pedersen {
     base: RistrettoPoint,
 }
 
-pub struct Commitment(CompressedRistretto);
+type Commitment = RistrettoPoint;
 
 impl Pedersen {
     pub fn new(size: usize) -> Self {
@@ -49,22 +49,94 @@ impl Pedersen {
         }
 
         let mut gens = self.generators.to_vec(); // boo-hoo
-        gens.iter().zip(scalars.iter()).map(|(g, a)| g * a);
+        gens = gens
+            .iter()
+            .zip(scalars.iter())
+            .map(|(g, a)| g * a)
+            .collect();
         let commitment = gens
             .iter()
             .fold(self.base * randomness, |accum, commitment| {
                 commitment + accum
             });
         println!("{commitment:?}");
-        Commitment(commitment.compress())
+        commitment
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use rand_core::OsRng;
+
     #[test]
     fn it_works() {
         let result = 2 + 2;
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn scalar_addition() {
+        let mut rng = OsRng;
+        let mut trapdoor = Pedersen::new(100);
+
+        let mut a_scalars: Vec<Scalar> = (0..100)
+            .into_iter()
+            .map(|_| Scalar::random(&mut rng))
+            .collect::<Vec<Scalar>>();
+
+        let mut b_scalars: Vec<Scalar> = (0..100)
+            .into_iter()
+            .map(|_| Scalar::random(&mut rng))
+            .collect::<Vec<Scalar>>();
+
+        let mut sum_scalars: Vec<Scalar> = b_scalars
+            .iter()
+            .zip(a_scalars.iter())
+            .map(|(a, b)| a + b)
+            .collect::<Vec<Scalar>>();
+
+        b_scalars
+            .iter()
+            .zip(a_scalars.iter())
+            .zip(sum_scalars.iter())
+            .for_each(|((&a, &b), &sum)| assert_eq!(a + b, sum));
+
+        let elem_a = a_scalars.get(10).unwrap().clone();
+        let elem_b = b_scalars.get(10).unwrap().clone();
+        let elem_c = sum_scalars.get(10).unwrap().clone();
+        assert_eq!(elem_a + elem_b, elem_c);
+    }
+
+    #[test]
+    fn homomorphic_addition() {
+        let mut rng = OsRng;
+        let mut trapdoor = Pedersen::new(100);
+        let r1 = Scalar::random(&mut rng);
+        let r2 = Scalar::random(&mut rng);
+
+        let mut a_scalars: Vec<Scalar> = (0..100)
+            .into_iter()
+            .map(|_| Scalar::random(&mut rng))
+            .collect::<Vec<Scalar>>();
+
+        let mut b_scalars: Vec<Scalar> = (0..100)
+            .into_iter()
+            .map(|_| Scalar::random(&mut rng))
+            .collect::<Vec<Scalar>>();
+
+        let mut sum_scalars: Vec<Scalar> = b_scalars
+            .iter()
+            .zip(a_scalars.iter())
+            .map(|(a, b)| a + b)
+            .collect::<Vec<Scalar>>();
+
+        let c1 = trapdoor.commit(a_scalars, r1);
+        let c2 = trapdoor.commit(b_scalars, r2);
+        let sum = c1 + c2;
+
+        let c3 = trapdoor.commit(sum_scalars, r1 + r2);
+
+        assert_eq!(sum.compress(), c3.compress());
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,52 @@
-use curve25519_dalek_ng::scalar::Scalar;
+use curve25519_dalek_ng::{ristretto::RistrettoPoint, scalar::Scalar};
 use pedersen_toy;
 use rand_core::OsRng;
 
 fn main() {
     let mut rng = OsRng;
-    let mut pedersen = pedersen_toy::Pedersen::new(100 as usize, &mut rng);
+
+    let mut s = Scalar::random(&mut rng);
+
+    println!("scalar: {s:?}");
+
+    let mut z = Scalar::zero();
+
+    println!("zero: {z:?}");
+
+    let mut prod = s * z;
+    println!("prod: {prod:?}");
+
+    let mut point = RistrettoPoint::random(&mut rng);
+    println!("point: {point:?}");
+
+    let mut prod_point = z * point;
+    println!("prod_point: {prod_point:?}");
+
+    let mut point2 = RistrettoPoint::random(&mut rng);
+    let mut prod_point2 = z * point2;
+
+    println!("prod_point2: {prod_point2:?}");
+
+    println!("canonical:");
+
+    let canon_point = prod_point.compress();
+    println!("prod_point_compress: {canon_point:?}");
+
+    let canon_point2 = prod_point2.compress();
+    println!("prod_point_compress2: {canon_point2:?}");
+
+    println!("rest of code!");
+
+    let mut pedersen = pedersen_toy::Pedersen::new(100 as usize);
 
     let mut scalars: Vec<Scalar> = Vec::with_capacity(100);
 
     for i in 0..100 {
         scalars.push(Scalar::random(&mut rng))
     }
+
+    let mut empty: Vec<Scalar> = Vec::new();
+    pedersen.commit(empty, Scalar::zero());
 
     let r = Scalar::random(&mut rng);
     pedersen.commit(scalars, r);
