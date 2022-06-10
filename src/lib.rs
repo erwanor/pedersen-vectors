53c76a01b13ed415d776e436f0413c568ab8258d
erwanor <erwan.ounn.84@gmail.com>
2022-06-10 11:47:07 +0200

    feat: rewrite `VectorCommitter::commit` to use `multiscalar_mut`

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,20 @@
-use std::num;
+use curve25519_dalek_ng::{ristretto::RistrettoPoint, scalar::Scalar, traits::MultiscalarMul};
 
-use curve25519_dalek_ng::{
-    ristretto::{self, CompressedRistretto, RistrettoPoint},
-    scalar::Scalar,
-};
-
-use rand::{CryptoRng, RngCore};
 use sha2::Sha512;
 
-// An instantiation of a Pedersen commitment schemes
-pub struct Pedersen {
+//replace with const generic
+pub struct VectorCommitter {
     // number of generators in the scheme
     commit_size: usize,
     // group generators
     generators: Vec<RistrettoPoint>,
-    // base
-    base: RistrettoPoint,
+    // blinding base 
+    base_blinding: RistrettoPoint,
 }
 
 type Commitment = RistrettoPoint;
 
-impl Pedersen {
+impl VectorCommitter {
     pub fn new(size: usize) -> Self {
         let mut generators = (0..size)
             .into_iter()
@@ -30,17 +24,20 @@ impl Pedersen {
             })
             .collect::<Vec<RistrettoPoint>>();
 
-        Pedersen {
+        let base_blinding =
+            RistrettoPoint::hash_from_bytes::<Sha512>("pedersen_domain_sep:H".as_bytes());
+
+        generators.push(base_blinding);
+
+        Self {
             commit_size: size,
             generators,
-            base: RistrettoPoint::hash_from_bytes::<Sha512>(
-                format!("pedersen_domain_sep:H").as_bytes(),
-            ),
+            base_blinding,
         }
     }
 
     // commit to N scalars, with randomness
-    pub fn commit(&self, mut scalars: Vec<Scalar>, randomness: Scalar) -> Commitment {
+    pub fn commit(&mut self, mut scalars: Vec<Scalar>, randomness: Scalar) -> Commitment {
         if scalars.len() > self.commit_size {
             unimplemented!("committing too many elements")
         }
@@ -51,21 +48,9 @@ impl Pedersen {
             .into_iter()
             .for_each(|_| scalars.push(Scalar::zero()));
 
-        let mut gens = self.generators.to_vec(); // boo-hoo
-        gens = gens
-            .iter()
-            .zip(scalars.iter())
-            .map(|(g, a)| g * a)
-            .collect();
+        scalars.push(randomness);
 
-        let commitment = gens
-            .iter()
-            .fold(self.base * randomness, |accum, commitment| {
-                commitment + accum
-            });
-
-        println!("{commitment:?}");
-        commitment
+        RistrettoPoint::multiscalar_mul(scalars, self.generators.to_owned())
     }
 }
 
@@ -81,57 +66,24 @@ mod tests {
     }
 
     #[test]
-    fn scalar_addition() {
-        let mut rng = OsRng;
-        let mut trapdoor = Pedersen::new(100);
-
-        let mut a_scalars: Vec<Scalar> = (0..100)
-            .into_iter()
-            .map(|_| Scalar::random(&mut rng))
-            .collect::<Vec<Scalar>>();
-
-        let mut b_scalars: Vec<Scalar> = (0..100)
-            .into_iter()
-            .map(|_| Scalar::random(&mut rng))
-            .collect::<Vec<Scalar>>();
-
-        let mut sum_scalars: Vec<Scalar> = b_scalars
-            .iter()
-            .zip(a_scalars.iter())
-            .map(|(a, b)| a + b)
-            .collect::<Vec<Scalar>>();
-
-        b_scalars
-            .iter()
-            .zip(a_scalars.iter())
-            .zip(sum_scalars.iter())
-            .for_each(|((&a, &b), &sum)| assert_eq!(a + b, sum));
-
-        let elem_a = a_scalars.get(10).unwrap().clone();
-        let elem_b = b_scalars.get(10).unwrap().clone();
-        let elem_c = sum_scalars.get(10).unwrap().clone();
-        assert_eq!(elem_a + elem_b, elem_c);
-    }
-
-    #[test]
+    // Check that com_ck(A, q) \cdot com_ck(B, r) = com_ck(A+B, q+r)
     fn homomorphic_addition() {
-        // todo clean up the iterator forest
         let mut rng = OsRng;
-        let mut trapdoor = Pedersen::new(100);
+        let mut trapdoor = VectorCommitter::new(100);
         let r1 = Scalar::random(&mut rng);
         let r2 = Scalar::random(&mut rng);
 
-        let mut a_scalars: Vec<Scalar> = (0..100)
+        let a_scalars: Vec<Scalar> = (0..100)
             .into_iter()
             .map(|_| Scalar::random(&mut rng))
             .collect::<Vec<Scalar>>();
 
-        let mut b_scalars: Vec<Scalar> = (0..100)
+        let b_scalars: Vec<Scalar> = (0..100)
             .into_iter()
             .map(|_| Scalar::random(&mut rng))
             .collect::<Vec<Scalar>>();
 
-        let mut sum_scalars: Vec<Scalar> = b_scalars
+        let sum_scalars: Vec<Scalar> = b_scalars
             .iter()
             .zip(a_scalars.iter())
             .map(|(a, b)| a + b)
@@ -144,5 +96,28 @@ mod tests {
         let c3 = trapdoor.commit(sum_scalars, r1 + r2);
 
         assert_eq!(sum.compress(), c3.compress());
+    }
+
+    #[test]
+    // Test vector across versions
+    fn consistent_vector() {
+        let blinding_scalar = Scalar::hash_from_bytes::<Sha512>(b"blinding_factor");
+
+        let scalar_preimages = vec![b"s1", b"s2", b"s3"];
+        let mut scalars = Vec::with_capacity(scalar_preimages.len());
+        for preimage in scalar_preimages.iter() {
+            scalars.push(Scalar::hash_from_bytes::<Sha512>(preimage.as_slice()));
+        }
+
+        let mut naive_trapdoor = VectorCommitter::new(scalar_preimages.len());
+        let naive_commitment = naive_trapdoor.commit(scalars, blinding_scalar).compress();
+        let compressed_commitment_test_vector = [
+            20, 32, 230, 13, 11, 148, 239, 179, 111, 36, 209, 242, 200, 173, 167, 30, 25, 202, 98,
+            143, 245, 64, 128, 210, 14, 1, 216, 208, 185, 226, 211, 12,
+        ];
+        assert_eq!(
+            naive_commitment.to_bytes(),
+            compressed_commitment_test_vector
+        );
     }
 }
