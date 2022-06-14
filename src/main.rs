7743848a2a9d85af9f1a00032c968d4ddfb8d6fc
erwanor <erwan.ounn.84@gmail.com>
2022-06-14 17:47:43 +0200

    fix: remove scratchwork

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+#![feature(stmt_expr_attributes)]
+
 use curve25519_dalek_ng::{ristretto::RistrettoPoint, scalar::Scalar};
-use pedersen_toy;
+use pedersen_toy::VectorCommitter;
 use rand_core::OsRng;
+
+use bulletproofs::PedersenGens;
+use sha2::{Digest, Sha512};
 
 fn main() {
     let mut rng = OsRng;
@@ -10,7 +15,6 @@ fn main() {
     println!("scalar: {s:?}");
 
     let mut z = Scalar::zero();
-
     println!("zero: {z:?}");
 
     let mut prod = s * z;
@@ -37,7 +41,7 @@ fn main() {
 
     println!("rest of code!");
 
-    let mut pedersen = pedersen_toy::Pedersen::new(100 as usize);
+    let mut pedersen = pedersen_toy::VectorCommitter::new(100 as usize);
 
     let mut scalars: Vec<Scalar> = Vec::with_capacity(100);
 
@@ -48,6 +52,55 @@ fn main() {
     let mut empty: Vec<Scalar> = Vec::new();
     pedersen.commit(empty, Scalar::zero());
 
-    let r = Scalar::random(&mut rng);
-    pedersen.commit(scalars, r);
+    let mut rng = OsRng;
+    // CompressedRistretto: [64, 149, 245, 9, 225, 129, 49, 23, 244, 236, 33, 7, 240, 236, 169, 254, 180, 2, 186, 136, 10, 147, 152, 15, 9, 68, 179, 163, 8, 124, 255, 85]
+
+    let B = RistrettoPoint::hash_from_bytes::<Sha512>("pedersen_domain_sep:0".as_ref());
+    let B_blinding = RistrettoPoint::hash_from_bytes::<Sha512>("pedersen_domain_sep:H".as_ref());
+    let mut dalek_pedersen = PedersenGens { B, B_blinding };
+
+    let blinding_scalar = Scalar::hash_from_bytes::<Sha512>(b"blinding_factor");
+
+    let preimages = vec![b"s1", b"s2", b"s3"];
+    let len = preimages.len();
+    let mut scalars = Vec::with_capacity(len);
+    for preimage in preimages {
+        scalars.push(Scalar::hash_from_bytes::<Sha512>(preimage));
+    }
+
+    let mut naive_trapdoor = VectorCommitter::new(len);
+    let naive_commitment = naive_trapdoor.commit(scalars, blinding_scalar);
+    let canonical_naive_commitment = naive_commitment.compress();
+    println!("canonical naive: {canonical_naive_commitment:?}");
+
+    let mut rng = OsRng;
+    let mut trapdoor = VectorCommitter::new(100);
+    let r1 = Scalar::random(&mut rng);
+    let r2 = Scalar::random(&mut rng);
+    let a_scalars: Vec<Scalar> = (0..100)
+        .into_iter()
+        .map(|_| Scalar::random(&mut rng))
+        .collect::<Vec<Scalar>>();
+
+    let b_scalars: Vec<Scalar> = (0..100)
+        .into_iter()
+        .map(|_| Scalar::random(&mut rng))
+        .collect::<Vec<Scalar>>();
+
+    let sum_scalars: Vec<Scalar> = b_scalars
+        .iter()
+        .zip(a_scalars.iter())
+        .map(|(a, b)| a + b)
+        .collect::<Vec<Scalar>>();
+    println!("0test..");
+
+    let c1 = trapdoor.commit(a_scalars, r1);
+    let c2 = trapdoor.commit(b_scalars, r2);
+    let sum = c1 + c2;
+    println!("test..");
+
+    let c3 = trapdoor.commit(sum_scalars, r1 + r2);
+    println!("checking..");
+    println!("{c3:?}");
+    println!("{sum:?}");
 }
